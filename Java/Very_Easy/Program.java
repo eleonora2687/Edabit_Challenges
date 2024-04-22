6dd420d459f58e6a32ecd68c893b1b97f341b805
cameronmcnz <you@example.com>
2024-04-22 11:02:34 +0300

    changed and added things

--- Java/Very_Easy/Program.java
+++ Java/Very_Easy/Program.java
@@ -6,6 +6,7 @@
 // }
 // All you have to do is type return true; between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
 
+
 public class Program {
     public static void main(String[] args) {
         returnTrue();
