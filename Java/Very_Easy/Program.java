dea8f0f8155fcc366d07ddc87148725bc2c84bb0
cameronmcnz <you@example.com>
2024-04-22 10:31:22 +0300

    "my commit"

---
+++ Java/Very_Easy/Program.java
@@ -0,0 +1,16 @@
+// How Edabit Works
+
+// This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+// public static boolean returnTrue() {
+
+// }
+// All you have to do is type return true; between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+
+public class Program {
+    public static void main(String[] args) {
+        returnTrue();
+    }
+    public static boolean returnTrue() {
+        return true;
+    }
+}
