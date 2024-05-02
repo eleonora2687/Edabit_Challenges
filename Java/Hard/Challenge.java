e2238fba6e09ca2b1dcdf50f10ebdc13aa544657
cameronmcnz <you@example.com>
2024-05-02 13:00:58 +0300

    Commit

--- Java/Hard/Challenge.java
+++ Java/Hard/Challenge.java
@@ -1,19 +1,20 @@
 // Seven Boom!
 // Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
 
-/* Examples
-sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
- */// 7 contains the number seven.
+// Examples
+// sevenBoom([1, 2, 3, 4, 5, 6, 7]) -> "Boom!"
+//  7 contains the number seven.
 
-// sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+
+// sevenBoom([8, 6, 33, 100]) -> "there is no 7 in the array"
 // None of the items contain 7 within them.
 
-// sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+// sevenBoom([2, 55, 60, 97, 86]) -> "Boom!"
 // 97 contains the number seven.
 
 
 
-public class Challenge {
+/* public class Challenge {
     public static void main(String[] args) {
         System.out.println(sevenBoom(new int[]{2, 6, 7, 9, 3}));
         System.out.println(sevenBoom(new int[]{33, 68, 400, 5}));
@@ -42,5 +43,123 @@ public class Challenge {
         
 	}    
 }
+ */
 
+
+
+
+//  Balanced Words
+
+//  We can assign a value to each character in a word, based on their position in the alphabet (a = 1, b = 2, ... , z = 26). A balanced word is one where the sum of values on the left-hand side of the word equals the sum of values on the right-hand side. For odd length words, the middle character (balance point) is ignored.
+
+//  Write a function that returns true if the word is balanced, and false if it's not.
+
+
+
+/* public class Challenge {
+    public static void main(String[] args) {
+        System.out.println(balanced("zips"));
+        System.out.println(balanced("brake"));
+        System.out.println(balanced("at"));
+        System.out.println(balanced("forgetful"));
+        System.out.println(balanced("vegetation"));
+        System.out.println(balanced("disillusioned"));
+        System.out.println(balanced("abstract"));
+        System.out.println(balanced("clever"));
+        System.out.println(balanced("seasoning"));
+        System.out.println(balanced("measure"));
+        System.out.println(balanced("conditionalities"));
+        System.out.println(balanced("uptight"));
+        System.out.println(balanced("ticket"));
+        System.out.println(balanced("calculate"));
+        System.out.println(balanced("join"));
+        System.out.println(balanced("anesthesiologies"));
+        System.out.println(balanced("command"));
+        System.out.println(balanced("graphite"));
+        System.out.println(balanced("quadratically"));
+        System.out.println(balanced("right"));
+        System.out.println(balanced("fossil"));
+        System.out.println(balanced("sparkling"));
+        System.out.println(balanced("dolphin"));
+        System.out.println(balanced("baseball"));
+        System.out.println(balanced("immense"));
+        System.out.println(balanced("pattern"));
+        System.out.println(balanced("hand"));
+        System.out.println(balanced("radar"));
+        System.out.println(balanced("oven"));
+        System.out.println(balanced("immutability"));
+        System.out.println(balanced("kayak"));
+        System.out.println(balanced("bartender"));
+        System.out.println(balanced("weight"));
+        System.out.println(balanced("lightbulbs"));
+        System.out.println(balanced("tail"));
+        
+    }
+    public static boolean balanced(String word) {
+          char [] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+          int first=0, second=0, temp=0;
+        
+          if (word.length()%2==0){
+          for (int i = 0; i <= (word.length() - 1)/2; i++) {
+
+            for (int j = 0; j < letters.length; j++) {
+                
+                if (word.charAt(i) == letters[j]) {
+                    temp=j+1;
+                    break;
+                }
+                
+            }first+=temp;
+          }
+
+
+          for (int i = word.length()/2; i <word.length(); i++) {
+            
+            for (int j = 0; j < letters.length; j++) {
+                if (word.charAt(i) == letters[j]) {
+                    temp=j+1;
+                    break;
+                }
+                
+            }second+=temp;
+          }
+          
+          if (first==second) {
+            return true;
+          }return false;
+        }
+        else{
+            for (int i = 0; i < (word.length() - 1)/2; i++) {
+
+                for (int j = 0; j < letters.length; j++) {
+                    
+                    if (word.charAt(i) == letters[j]) {
+                        temp=j+1;
+                        break;
+                    }
+                    
+                }first+=temp;
+              }
+    
+    
+              for (int i = word.length()/2+1; i <word.length(); i++) {
+                
+                for (int j = 0; j < letters.length; j++) {
+                    if (word.charAt(i) == letters[j]) {
+                        temp=j+1;
+                        break;
+                    }
+                    
+                }second+=temp;
+              }
+
+              if (first==second) {
+                return true;
+              }return false;
+            }
+
+        }
+    }
+
+     */
 
--- Python/Hard/Interview.py
+++ Python/Hard/Interview.py
@@ -49,7 +49,7 @@ Divide two numbers.
 
 """             
                 
-class Calculator:
+""" class Calculator:
     def add(self, a, b):
         return a + b
     
@@ -78,4 +78,106 @@ print(calculator.multiply(100,5))
 print(calculator.divide(10,5))
 print(calculator.divide(100,5))
         
-        
+         """
+         
+         
+
+""" Loves Me, Loves Me Not...
+
+"Loves me, loves me not" is a traditional game in which a person plucks off all the petals of a flower one by one, saying the phrase "Loves me" and "Loves me not" when determining whether the one that they love, loves them back.
+
+Given a number of petals, return a string which repeats the phrases "Loves me" and "Loves me not" for every alternating petal, and return the last phrase in all caps. Remember to put a comma and space between phrases.
+
+ """
+ 
+ 
+""" def loves_me(n):
+    s=""
+    if n==1:
+        s+="LOVES ME"
+    else:
+        for i in range(n-1):
+            if i%2==0:
+                s+="Loves me"
+            else:
+                s+="Loves me not"
+            s+=", "
+        if i%2==1:
+            s+="LOVES ME"
+        else:
+            s+="LOVES ME NOT"
+    return s
+
+print(loves_me(1))
+print(loves_me(2))
+print(loves_me(3))
+print(loves_me(4))
+print(loves_me(5))
+print(loves_me(6))
+print(loves_me(7))
+print(loves_me(8))
+print(loves_me(9))
+print(loves_me(10))
+print(loves_me(11))
+print(loves_me(12))
+print(loves_me(13))
+print(loves_me(14))
+print(loves_me(15))
+print(loves_me(16))
+print(loves_me(17))
+print(loves_me(18))
+print(loves_me(19))
+print(loves_me(20))
+print(loves_me(21))
+print(loves_me(22))
+print(loves_me(23))
+print(loves_me(24))
+print(loves_me(25))
+print(loves_me(26))
+print(loves_me(27))
+print(loves_me(28))
+print(loves_me(29))
+print(loves_me(30))
+print(loves_me(31))
+print(loves_me(32))
+print(loves_me(33))
+print(loves_me(34))
+print(loves_me(35))
+print(loves_me(36))
+print(loves_me(37))
+print(loves_me(38))
+print(loves_me(39))
+print(loves_me(40))
+print(loves_me(41))
+print(loves_me(42))
+print(loves_me(43))
+print(loves_me(44))
+print(loves_me(45))
+print(loves_me(46))
+print(loves_me(47))
+print(loves_me(48))
+print(loves_me(49))
+print(loves_me(50))
+print(loves_me(51))
+print(loves_me(52))
+print(loves_me(53))
+print(loves_me(54))
+print(loves_me(55))
+print(loves_me(56))
+print(loves_me(57))
+print(loves_me(58))
+print(loves_me(59))
+print(loves_me(60))
+print(loves_me(61))
+print(loves_me(29))
+print(loves_me(62))
+print(loves_me(63))
+print(loves_me(64))
+print(loves_me(65))
+print(loves_me(66))
+print(loves_me(67))
+print(loves_me(68))
+print(loves_me(69))
+print(loves_me(70))
+ """
+ 
